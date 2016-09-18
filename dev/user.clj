6705a9e71a35c661f9badce9dfe49728d342edc6
Chris Thompson <teaforthecat@gmail.com>
2016-09-18 12:10:41 -0500

    version 0.1.3

--- dev/user.clj
+++ dev/user.clj
@@ -15,15 +15,16 @@
     ;; must return the stream
     output-stream))
 
-(defn sieve [ctx]
-  {:salutations "Greetings"})
+(defn login [args req]
+  (if (= (:password args) (:username args))
+    {:anything "to-identify"}
+    nil ;; will return 401
+    ))
 
 (defn start []
-  (http/build-system sys {:http/handlers {:login sieve
-                                          :query-schema {(s/optional-key :accounts) s/Any
-                                                         (s/optional-key :account) s/Int}
-                                          :query-handler (fn [args auth-info]
-                                                           [args auth-info])
+  (http/build-system sys {:http/handlers {:login [{:username s/Str :password s/Str} login]
+                                          :query [{:q s/Any} (fn [args auth-info req]
+                                                               [args auth-info])]
                                           :event-stream event-stream-handler}
                                         ;(bones.http.auth/gen-secret)
                           :http/auth {:secret "CypOW2ZYqvB42ahTI9GdXZ5v4sphlwdC"
@@ -35,14 +36,9 @@
   (http/stop sys))
 
 (comment
-  (:conf @sys)
-  (:shield @sys)
-  (:shield (:routes @sys))
-;;
+;; SSE curl
 ;; curl "localhost:8080/api/events" --cookie "bones-session=w0Sr6aFcdwmtcnbmuxfmiuYgtmo0VUGylXKtdpVpv%2B7c5y3FVjWYdJ21cgv2g%2BlNkgdyYvE%2BvpqQWHUW%2BhcobzDOqd8M6SIWVWtp%2FXP9wG4%3D--fTYQgX1%2BSQkz2Q%2Bc5qaEhYGPMlBU2hdcdSy5bcs%2B2S0%3D" -v
-  ;; curl "localhost:3000/api/ws" --cookie "bones-session=w0Sr6aFcdwmtcnbmuxfmiuYgtmo0VUGylXKtdpVpv%2B7c5y3FVjWYdJ21cgv2g%2BlNkgdyYvE%2BvpqQWHUW%2BhcobzDOqd8M6SIWVWtp%2FXP9wG4%3D--fTYQgX1%2BSQkz2Q%2Bc5qaEhYGPMlBU2hdcdSy5bcs%2B2S0%3D" -v  -H "Connection: Upgrade" -H "Upgrade: websocket"
 
-  ;; curl "localhost:3000/api/ws" -v -H "Authorization: Token $TOKEN" -H "Connection: Upgrade" -H "Upgrade: websocket"
 (println "hi")
 (start)
 (stop)
