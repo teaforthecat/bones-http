b81716d71641a88dac735d8fd8eb1449bd7cbd70
Chris Thompson <teaforthecat@gmail.com>
2016-09-25 18:24:04 -0500

    add schema validation to conf

--- src/bones/http/core.clj
+++ src/bones/http/core.clj
@@ -3,14 +3,43 @@
             [bones.http.auth :as auth]
             [bones.http.handlers :as handlers]
             [bones.http.commands]
-            [bones.http.service :as service]))
+            [bones.http.service :as service]
+            [schema.core :as s]))
+
+(defn validate [conf]
+  (let [handlers (:http/handlers conf)
+        {:keys [commands query login]} handlers
+        name-schema (s/one s/Keyword :name)
+        schema-schema (s/one (s/protocol s/Schema) :schema)
+        ;; todo: figure out how to check for an fn; this handler schema is broken
+        handler-schema (s/cond-pre s/Keyword s/Symbol s/Any)]
+    ;; commands: [[name schema handler]]
+    (s/validate [[name-schema
+                  schema-schema
+                  (s/maybe handler-schema)]]
+                commands)
+    ;; query: [schema handler]
+    (s/validate (s/maybe  [schema-schema
+                           handler-schema])
+                query)
+    ;; login: [schema handler]
+    (s/validate (s/maybe [schema-schema
+                          handler-schema])
+                login)
+
+
+    conf))
 
 (defn build-system [sys conf]
-  (swap! sys #(-> %
-                  (assoc :conf conf)
-                  (assoc :shield (component/using (auth/map->Shield {}) [:conf]) )
-                  (assoc :routes (component/using (handlers/map->App {}) [:conf :shield]))
-                  (assoc :http   (component/using (service/map->Server {}) [:conf :routes])))))
+  ;; simplify the api even more by not requiring a system-map from the user
+  {:pre [(instance? clojure.lang.Atom sys)
+         (instance? clojure.lang.Associative conf)]}
+    (swap! sys #(-> (apply component/system-map (reduce concat %)) ;; if already a system-map break apart and put back together
+                    (assoc :conf (validate conf))
+                    (assoc :shield (component/using (auth/map->Shield {}) [:conf]) )
+                    (assoc :routes (component/using (handlers/map->App {})
+                                                    [:conf :shield]))
+                    (assoc :http   (component/using (service/map->Server {}) [:conf :routes])))))
 
 (defn start-system [system & components]
   (swap! system component/update-system components component/start))
--- test/bones/http/core_test.clj
+++ test/bones/http/core_test.clj
@@ -1,6 +1,8 @@
 (ns bones.http.core-test
   (:require [bones.http.core :as http]
-            [clojure.test :refer [deftest testing is]]))
+            [clojure.test :refer [deftest testing is]]
+            [com.stuartsierra.component :as component]
+            [schema.core :as s]))
 
 (def sys (atom {}))
 (def conf {:http/auth
@@ -14,3 +16,41 @@
     (is (= "a 16 byte stringa 32 byte string" (get-in @sys [:routes :shield :secret])))
     (is (= "pizza" (get-in @sys [:routes :shield :cookie-opts :cookie-name])))
     (http/stop sys)))
+
+(deftest validate-configuration
+  (testing "missing a schema"
+    (let [commands [[:a]]
+          config (assoc-in conf [:http/handlers :commands] commands)]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (http/build-system sys config)))))
+  (testing "a valid command"
+    (let [commands [[:a {:abc s/Str}]]
+          config (assoc-in conf [:http/handlers :commands] commands)]
+      (is (satisfies? component/Lifecycle
+                      (http/build-system sys config)))))
+  (testing "a command with an explicit handler"
+    (let [commands [[:a {:abc s/Str} ::a]
+                    [:a {:abc s/Str} 'a]]
+          config (assoc-in conf [:http/handlers :commands] commands)]
+      (is (satisfies? component/Lifecycle
+                      (http/build-system sys config)))))
+  (testing "missing a query schema"
+    (let [query [(fn [req] "hi")]
+          config (assoc-in conf [:http/handlers :query] query)]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (http/build-system sys config)))))
+  (testing "a valid query schema"
+    (let [query [{s/Any s/Any} (fn [req] "hi")]
+          config (assoc-in conf [:http/handlers :query] query)]
+      (is (satisfies? component/Lifecycle
+                      (http/build-system sys config)))))
+  (testing "missing a login schema"
+    (let [login [(fn [req] "hi")]
+          config (assoc-in conf [:http/handlers :login] login)]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (http/build-system sys config)))))
+  (testing "a valid login schema"
+    (let [login [{s/Any s/Any} (fn [req] "hi")]
+          config (assoc-in conf [:http/handlers :login] login)]
+      (is (satisfies? component/Lifecycle
+                      (http/build-system sys config))))))
